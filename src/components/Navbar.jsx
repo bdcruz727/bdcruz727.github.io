a73eda239a908b7379e703ed20dd99117320287c
bdcruz727 <bryandelacz@gmail.com>
2025-06-17 18:56:50 -0700

    redeploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
  
  export const Navbar = () =>{
 
@@ -5,14 +7,12 @@
     <nav className="fixed top-0 w-full z-40 backdrop-blur-lg border-b border-white/10 shadow-lg">
         <div className="max-w-5xl mx-auto px-4">
             <div className="flex justify-between items-center h-16">
+
                 <a href="#home" className="font-bold text-xl text-black">
                     {""}
-                    bdcruz<span className="text-blue-500">727</span> {""}
+                    wysi<span className="text-blue-500">727</span> {""}
                 </a>
 
-                <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden">
-                    &#9776;
-                </div>
 
                 <div className="hidden md:flex items-center space-x-8">
                     <a href="#home" className="font-bold hover:text-miku-blue transition-colors">
