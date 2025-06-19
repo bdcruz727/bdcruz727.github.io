6b0c4959bfbb1d145c150e551332a7f7d344720e
bdcruz727 <bryandelacz@gmail.com>
2025-06-19 13:17:42 -0700

    Added images to Contact, email icon, and centering fix

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,3 +1,5 @@
+import { MdEmail } from "react-icons/md";
+
 export const Contact = () => {
 
     const copyEmail = () => {
@@ -12,13 +14,25 @@ export const Contact = () => {
             <h2 className="text-5xl font-bold mb-8 text-miku-gray text-center p-6">
                 Contact Me!
             </h2>
-
-            <div className="rounded-xl p-8 border-miku-gray border hover:-translate-y-1 transition all" onClick={copyEmail}>
-                    <p className="text-miku-gray mb-2 text-2xl ">
+            <div className="flex justify-center items-center gap-12">
+                <img src="/images/christoff.jpg" alt="A picture of my dog"
+                className="w-96 h-96 object-cover rounded-full shadow-lg border-4 border-miku-sakura hover:-translate-y-1 transition-all">
+                </img>
+                <div className="flex flex-col text-miku-gray items-center justify-center text-4xl text-center cursor-pointer hover:-translate-y-1 transition all" onClick={copyEmail}>
+                    <MdEmail />
+                    <p>
                         bryandelacz@gmail.com
 
                         
                     </p>
+                </div>
+                    
+               
+
+                <img src="/images/moby.jpg" alt="A picture of my friend's cat"
+                className="w-96 h-96 object-cover rounded-full shadow-lg border-4 border-miku-sakura hover:-translate-y-1 transition-all">
+                </img>
+                
             </div>
 
         </div>
